77f038b62f8042e017307fd85118c51a4abf9571
Burekboi <mehmetmetenak@gmail.com>
2025-05-08 15:49:42 +0300

    loglama işia halloldu ana dizine logs/app.log içine tüm request ve responseler yazılıyor

---
+++ src/main/java/com/ders/bm470/config/LoggingInterceptor.java
@@ -0,0 +1,39 @@
+package com.ders.bm470.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Enumeration;
+
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) {
+
+        logger.info("İSTEK: {} {}", request.getMethod(), request.getRequestURI());
+
+        Enumeration<String> paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String name = paramNames.nextElement();
+            String value = request.getParameter(name);
+            logger.info("Parametre: {} = {}", name, value);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception ex) {
+        logger.info("YANIT DURUMU: {} {}", response.getStatus(), request.getRequestURI());
+    }
+}
